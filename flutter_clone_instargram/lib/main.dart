94a04284d323accd3ec5762a66b0ad702e50e32e
brojoon <ydngjink1234@gmail.com>
2023-12-31 01:26:46 +0900

    feat: BottomNavigation + History

---
+++ flutter_clone_instargram/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clone_instargram/src/app.dart';
+import 'package:flutter_clone_instargram/src/binding/init_bindings.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          titleTextStyle: TextStyle(color: Colors.black),
+        ),
+      ),
+      initialBinding: InitBinding(),
+      home: const App(),
+    );
+  }
+}
---
+++ flutter_clone_instargram/lib/src/app.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clone_instargram/src/controller/bottom_nav_controller.dart';
+import 'package:flutter_clone_instargram/src/pages/home.dart';
+import 'package:get/get.dart';
+import 'components/image_data.dart';
+
+class App extends GetView<BottomNavController> {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        child: Obx(
+            () =>
+        Scaffold(
+          body: IndexedStack(
+            index: controller.pageIndex.value,
+            children: [
+              const Home(),
+              Container(child: Center(child: Text('SEARCH'))),
+              Container(child: Center(child: Text('UPLOAD'))),
+              Container(child: Center(child: Text('ACTIVITY'))),
+              Container(child: Center(child: Text('MYPAGE'))),
+            ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            currentIndex: controller.pageIndex.value,
+            elevation: 0,
+              onTap: controller.changeBottomNav,
+            items: [
+              BottomNavigationBarItem(
+                  icon: ImageData(IconsPath.homeOff),
+                  activeIcon: ImageData(IconsPath.homeOn),
+                  label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: ImageData(IconsPath.searchOff),
+                activeIcon: ImageData(IconsPath.searchOn),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: ImageData(IconsPath.uploadIcon),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: ImageData(IconsPath.activeOff),
+                activeIcon: ImageData(IconsPath.activeOn),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  width: 30,
+                  height: 30,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.grey,
+                  ),
+                ),
+                label: 'home',
+              ),
+            ]
+          ),
+        )),
+        onWillPop: controller.willPopAction,
+    );
+  }
+}
